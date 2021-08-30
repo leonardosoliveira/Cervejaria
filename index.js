6d69d8999fc72c0f497bf6e76ab99890c49fb36e
wagnerrafa <wagner_rafa@hotmail.com.br>
2021-08-30 11:48:21 -0300

    (#01) include get list atendimento, procfile to heroku

--- index.js
+++ index.js
@@ -3,18 +3,24 @@ const customExpress = require('./config/customExpress')
 const Tabelas = require('./infraestrutura/tabelas')
 const conexao = require("./infraestrutura/conexao");
 
-conexao.connect(erro => {
-    if(erro){
-        console.log(erro)
-    }else{
 
-        console.log('conectado com sucesso')
-        
-        Tabelas.init(conexao)
-        
-        const app = customExpress()
+function handle() {
+    conexao.connect(erro => {
+        if (erro) {
+            console.log(erro)
+            setTimeout(handle, 2000);
+        } else {
 
-        app.listen(process.env.PORT || 3000, () => console.log('Servidor rodando na porta 3000'))
+            console.log('conectado com sucesso')
 
-    }
-})
+            Tabelas.init(conexao)
+
+            const app = customExpress()
+
+            app.listen(process.env.PORT || 3000, () => console.log('Servidor rodando na porta 3000'))
+
+        }
+
+    })
+}
+handle()
